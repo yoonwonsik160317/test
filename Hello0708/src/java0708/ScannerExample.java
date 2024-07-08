e2525b3191fc1ff694a706b890953956f08053e8
yoonwonsik160317 <wssesang@gmail.com>
2024-07-08 17:25:08 +0900

    first commit

---
+++ Hello0708/src/java0708/ScannerExample.java
@@ -0,0 +1,21 @@
+package java0708;
+
+import java.util.Scanner;
+//주석
+public class ScannerExample {
+	public static void main(String[] args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+		String inputData;
+		
+		while(true) {
+			inputData = scanner.nextLine();	
+			System.out.println("입력된 문자열: \"" + inputData +"\"");
+			if(inputData.equals("q") ) {
+				break;
+			}
+		}
+		
+		
+		System.out.println("종료");
+	}
+}
---
+++ Hello0708/src/java0708/Test.java
@@ -0,0 +1,9 @@
+package java0708;
+
+public class Test {
+	public void main(String[] args) {
+		System.out.println("깃 테스트 ");
+		
+	}
+	
+}
